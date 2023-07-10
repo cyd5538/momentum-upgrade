ed08b42277ade8a48b17089a75ed5dab73445421
cyj <cyd5538@naver.com>
2023-07-10 19:54:07 +0900

    todo section bg text color 변경

--- src/components/Home/ListSectionHeader.tsx
+++ src/components/Home/ListSectionHeader.tsx
@@ -6,9 +6,9 @@ interface ListSectionHeaderProps {
 
 const ListSectionHeader:React.FC<ListSectionHeaderProps> = ({ text, bg, count }) => {
   return (
-    <div className={`${bg} flex items-center h-12 shadow-md rounded-md uppercase text-sm text-white pl-2`}>
+    <div className={`bg-white flex items-center h-12 shadow-md rounded-md uppercase text-sm text-black pl-2`}>
       {text}
-      <div className="ml-2 bg-white w-5 h-5 text-black rounded-md p-2 flex items-center justify-center">{count}</div>
+      <div className="ml-2 bg-black w-5 h-5 text-white rounded-md p-2 flex items-center justify-center">{count}</div>
     </div>
   )
 }
